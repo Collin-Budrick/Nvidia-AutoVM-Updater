0dc613afa52a49dd4a93b92a6ead3533892b1a04
quantum7 <collin.budrick@gmail.com>
2021-07-30 14:37:00 -0400

    Automake - Share file

--- AutoVMUpdater.bat
+++ AutoVMUpdater.bat
@@ -1,3 +1,5 @@
 echo off
-python "\\Quantum7\Share\Maintenance\autovm-updater.py"
+for /f "tokens=*" %%a in ('powershell -Command "& {Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters'  | Select-Object HostName}"') do set hostvm=%%a
+net use Z: \\%hostvm%\DriverHub /persistent:yes
+python "\\%hostvm%\DriverHub\autovm-updater.py"
 exit /b
