db9e8fab2d2fab3a9d3d0ce156b92fd4ebb77c8f
quantum7 <76412116+Collin-Budrick@users.noreply.github.com>
2021-07-29 23:33:16 -0400

    OVERHALL - Bug fix

--- autovm-updater.py
+++ autovm-updater.py
@@ -1,12 +1,13 @@
 from os import remove, rmdir, walk, mkdir
 from shutil import copy2, rmtree, copytree
 
-check = "nv_dispi.inf_amd64_"
-syspath = "C:/Windows/System32/"
 sharepath = "//Quantum7/Share/Cuda_Drivers/" # Edit to path of Share drive
+syspath = "C:/Windows/System32/"
+check = "nv_dispi.inf_amd64_"
 folders = []
 direc = []
 dlls = []
+existing = []
 needed_dlls = {
    'NvAgent.dll',
    'nvapi64.dll',
@@ -18,14 +19,12 @@ needed_dlls = {
    'nvcuvid.dll'
 }
 
-def diff(li1, li2): 
-    return list(set(li1)-set(li2))+list(set(li2)-set(li1))
-
-# What directory exists in 
+# What directory exists in FileRepository:
 for (dirpath, dirnames, filenames) in walk(syspath+'HostDriverStore/FileRepository/'):
     direc.extend(dirnames)
     nvidiafilepres = [i for i in direc if i.startswith(check)]
-existing = nvidiafilepres.pop()
+    try: existing = nvidiafilepres.pop()
+    except: continue
 
 # Get a list current files and directory's in Share drive:
 for (dirpath, dirnames, filenames) in walk(sharepath):
@@ -35,20 +34,16 @@ for (dirpath, dirnames, filenames) in walk(sharepath):
 file = nvidiafile.pop()
 
 # Removing the existing folder if not the same as in sharepath:
-if file != existing: [rmtree(syspath+'HostDriverStore')]
+if file != existing:
+    try: [rmtree(syspath+'HostDriverStore')]
+    except: [print("Removed old files...")]
 else: [print("Already Updated - Nothing to do.")] [exit(0)]
-
-# Compare available files with required:
-if (diff(dlls, needed_dlls)) == [ ]: [print('Deleting old Nvidia DLL files in System32...')]
-else:
-    needed = (diff(dlls, needed_dlls))
-    print("ERROR: following is needed in "+sharepath+" for successful install: ")
-    for i in needed: [print(i + "\r")] [exit(1)]
 
 # Try to delete old Nvidia DLL files in System32:
 for i in needed_dlls:
-    remove(syspath+i)
+    try: [remove(syspath+i)]
+    except: [print("Couldn't find: "+i+" in "+syspath)]
     copy2(str(sharepath+i), str(syspath)) # Copy new Nvidia DLL files from Share drive
 print("Copied new Nvidia DLL files from: "+sharepath)
 copytree(str(sharepath+file), str(syspath+"HostDriverStore/FileRepository/"+file))
-print("Completed.") [exit(0)]
+print("Copied: "+file+" to "+syspath+"\nCompleted.") [exit(0)]
